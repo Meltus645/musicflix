2bf711fe1dde9b1a0e506b5da4c9ae1bc34a6a41
=wajuma <=meltusjuma5@gmail.com>
2021-03-03 09:55:14 +0300

    track label

--- index.py
+++ index.py
@@ -4,5 +4,8 @@ from tkinter import  Tk, Label, Button, filedialog
 # screen 
 
 master =Tk()
-master.title("Music Player")
+master.title("Musiflix Player")
+
+Label(master, text="Music Player", font=("Calibri", 15), fg="red").grid(sticky="N", row=0,padx=120)
+Label(master, text="Select track", font=("Calibri", 12), fg="blue").grid(sticky="N", row=1,padx=120)
 master.mainloop()
