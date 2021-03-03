2f72d29f95808878e64ffebd59d7232bec7996aa
=wajuma <=meltusjuma5@gmail.com>
2021-03-03 10:40:39 +0300

    volume lable

--- index.py
+++ index.py
@@ -8,7 +8,8 @@ master.title("Musiflix Player")
 
 #labels
 Label(master, text="Music Player", font=("Calibri", 15), fg="red").grid(sticky="N", row=0,padx=120)
-Label(master, text="Select a track", font=("Calibri", 12), fg="blue").grid(sticky="N", row=1,padx=120)
+Label(master, text="Please select a track", font=("Calibri", 12), fg="blue").grid(sticky="N", row=1,padx=120)
+Label(master, text="Volume", font =("Calibri",12),fg="red").grid(sticky="N", row=4)
 song_title =Label(master, font =("Calibri",12))
 song_title.grid(sticky="N", row =3);
 volume_label =Label(master, font =("Calibri",12))
