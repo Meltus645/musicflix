6a27937d2cdd047697401b5f834d182df8e3edc9
=wajuma <=meltusjuma5@gmail.com>
2021-03-03 10:26:26 +0300

    select buton

--- index.py
+++ index.py
@@ -6,10 +6,14 @@ from tkinter import  Tk, Label, Button, filedialog
 master =Tk()
 master.title("Musiflix Player")
 
+#labels
 Label(master, text="Music Player", font=("Calibri", 15), fg="red").grid(sticky="N", row=0,padx=120)
 Label(master, text="Select a track", font=("Calibri", 12), fg="blue").grid(sticky="N", row=1,padx=120)
 song_title =Label(master, font =("Calibri",12))
 song_title.grid(sticky="N", row =3);
 volume_label =Label(master, font =("Calibri",12))
 volume_label.grid(sticky="N", row =5);
+
+#buttons 
+Button(master, text="select song", font=("Calibri",12),).grid(sticky="N", row=2)
 master.mainloop()
